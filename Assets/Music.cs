28c358107765bdc001c42c23a079eca4d98adfd3
Nubar10 <32017169+Nubar10@users.noreply.github.com>
2017-11-03 13:32:58 +0400

    Initial commit

---
+++ Assets/Music.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music10 : MonoBehaviour
+{
+
+
+    static Music10 instance = null;
+    private void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+
+        }
+
+
+    }
+
+}
---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music1 : MonoBehaviour
+{
+   
+       
+    static Music1 instance = null;
+    private void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+
+        }
+
+      
+    }
+
+}
